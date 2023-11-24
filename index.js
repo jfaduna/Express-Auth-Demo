06ad4bf27502216a2525ee7ea0b0089903d9dc72
jfaduna <flomeraduna@gmail.com>
2023-11-24 15:36:56 +1100

    add: signup feature

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express();
+const User = require('./models/user')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+mongoose.connect('mongodb://localhost:27017/authDemo');
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected")
+})
+
+app.set('view engine', 'ejs')
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Home page')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username: username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/')
+})
+
+app.get('/secret', (req, res) => {
+    res.send('You must be logged in to see this page.')
+})
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
